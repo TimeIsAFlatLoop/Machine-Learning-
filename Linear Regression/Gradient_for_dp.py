3897c977b9e2880172cb1cd56d74ed8b491746b6
DeepSouthLogic <aqsagotanerror@gmail.com>
2025-04-30 13:50:52 +0600

    Update Gradient_for_dp.py

--- Linear Regression/Gradient_for_dp.py
+++ Linear Regression/Gradient_for_dp.py
@@ -1,27 +1,28 @@
 #Pizza's size X
-x = [0.6, 0.8, 0.12, 0.14, 0.18] 
+x = [6, 8, 12, 14, 18] 
 #Pizza's prices  
 y = [350, 775, 1150,1395,1675]
-m, b = 0.0,0.0
-alpha = 0.1 
-iterations = 100
+m, b = 0,0
+alpha = 0.01 
+iterations = 10000
 n = len(x)
 
 for _ in range(iterations):
-    gd_m = 0.0
-    gd_b = 0.0 
-    
+    gd_m = 0
+    gd_b = 0 
+    cost = 0
     for i in range(n):
         pred_y = m * x[i] + b 
         errors = y[i] - pred_y
-        
+        cost += (errors) ** 2 
         gd_m +=(-1/n) * x[i] * errors
         
         gd_b += (-1/n) * errors
         
-        m = m - alpha * gd_m
-        b = m - alpha * gd_b
-        
-size = 17/10 
+    m = m - alpha * gd_m
+    b = b - alpha * gd_b
+    cost = cost / (2*n)
+print(f"Cost {cost}")    
+size = 17
 pred_y = m * size + b
 print(round(pred_y))
